c4587235a24e31b10241a3fccec350974c5e8b68
vagant1987 <vogant87@mail.ru>
2024-08-23 14:29:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+name = 'Sergey'
+print(('Name:') + (name))
+age = 36
+print(('Age:') + str(age))
+new_age = 37
+print(('New Age:') + str(new_age))
+is_student = True
+is_student = str(is_student)
+print(('Is Student:') + (is_student))
+
